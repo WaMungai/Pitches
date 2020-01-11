3e83e4f29a149c9dab18cb2b073e042971ca11a0
Joyce <wamungai29@gmail.com>
2020-01-12 00:04:54 +0300

    Input logic for pitch,updateprofile and comment forms.

---
+++ app/main/forms.py
@@ -0,0 +1,26 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
+from wtforms.validators import Required 
+
+class PitchForm(FlaskForm):
+    '''
+    Defines pitch column names and their data values
+    '''
+    title =StringField('Pitch title',validators =[Required()])
+    text =TextAreaField('Text',validators=[Required()])
+    cartegory= SelectField('Type',choices[('pickup-line','Pickup-line pitches'),('interview','Interview pitch','product''Product pitch'),('promotion','Promotion pitch')],validators =[Required()])
+    submit=SubmitField('Submit')
+    
+class UpdateProfile(FlaskForm):
+    '''
+    Class that updates user profile
+    '''
+    bio =TextAreaField('Bio',validators=[Required()])
+    submit =SubmitField('Submit')
+    
+class CommentForm(FlaskForm):
+    '''
+    Defines columns for the comment form section
+    '''
+    text =TextAreaField('Leave a comment:',validators =[Required()])
+    submit =SubmitField('Submit')
