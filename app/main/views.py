e138672f5e2724b28cc01e9b9aa6a7e27b5b29ee
Joyce <wamungai29@gmail.com>
2020-01-12 10:04:44 +0300

    Add views for pitches based on category.

--- app/main/views.py
+++ app/main/views.py
@@ -34,3 +34,39 @@ def profile(uname):
             
             db.session.add(user)
             db.session.commit()
+            
+            return redirect(url_for('profile/update.html',form=form))
+        
+@main.route('/pitch/new',mrthods =['GET','POST'])
+@login_required
+def new_pitches():
+    pitch_form =PitchForm()
+    if pitch_form.validate_on_submit():
+        title =pitch_form.title.data
+        pitch =pitch_form.txt.datacategory = pitch_form.cartegory.data
+        
+        new_pitch =Pitch(title =tile,content =pitch,category=category,user=current_user,lokes=0,dislikes=0)
+        
+        new_pitch.save_pitch()
+        return redirect(url_for('.index'))
+    
+    title ='New Pitch'
+    return render_template('new_pitch.html',title=title,pitch_form=pitch_form)
+
+@main.route('/pitches/interview_pitches')
+def interview_pitches():
+    pitches=Pitch.get_pitches('interview')
+    
+    return render_template("interview_piches.html",pitches=pitches)
+
+@main.route('/pitches/product_pitches')
+def product_pitches():
+    pitches =Pitch.get_pitches('product')
+    
+    return render_template('product_pitches.html',pitches=pitches)
+
+@main.route('/pitches/promotion_pitches')
+def promotion_pitches():
+    pitches =Pitch.get_pitches('promotion')
+    
+    return render_template('promotion_pitches.html',pitches =pitches)
