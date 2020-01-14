bf1ff8be53d43faaf56212b02809fc1b129af95b
Joyce <wamungai29@gmail.com>
2020-01-14 04:04:53 +0300

    Correct typo

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -14,13 +14,13 @@ class RegistrationForm(FlaskForm):
     
     def validate_email(self,data_field):
         if User.query.filte_by(email=data_field.data).first():
-            raise ValidationErrord('There is an account with that email')
+            raise ValidationError('There is an account with that email')
         
     def validate_username(self,data_field):
-        if User.qury.filter_by(username=data_field.data).first():
+        if User.query.filter_by(username=data_field.data).first():
             raise Validatiinerror('That username is taken')
         
-class Login Form(FlaskForm):
+class LoginForm(FlaskForm):
     email =StringField('Your Email Address',validators =[Required(),Email()])
     password =PasswordField('Password',validators=[Required()])
     remember =BooleanField('Remember me')
